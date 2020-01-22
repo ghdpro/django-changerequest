e7d58bd317f6fbdf9503e3d72ae1650e7d12a06f
Gerard Krijgsman <2293043+ghdpro@users.noreply.github.com>
2020-01-22 17:23:29 +0100

    Carefully re-implementing functionality

--- changerequest/models.py
+++ changerequest/models.py
@@ -2,15 +2,148 @@
 
 import threading
 
+from django.conf import settings
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
+
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.postgres.fields import JSONField
+
+from .utils import format_object_str, model_to_dict, changed_keys, filter_data, get_ip_from_request
 
 
 class ChangeRequest(models.Model):
-    thread = threading.local()
+    thread = threading.local()  # Used to store request via middleware
+
+    class Type:
+        """Constants that define actions for which ChangeRequest records can be saved
+
+        The first three are obvious; 'Related' is used for associated data that usually
+        has a 1-N relation with the main model for which history is being saved.
+        """
+        ADD = 1
+        MODIFY = 2
+        DELETE = 3
+        RELATED = 4
+        choices = (
+            (ADD, 'Add'),
+            (MODIFY, 'Modify'),
+            (DELETE, 'Delete'),
+            (RELATED, 'Related')
+        )
+
+    class Status:
+        """Constants that define the status of a ChangeRequest
+
+        Pending    = ChangeRequest is awaiting moderation; data is not yet committed to the database
+        Approved   = ChangeRequest has been (automatically) approved and data is committed to the database
+        Denied     = A moderator has denied the ChangeRequest
+        Withdrawn  = The user has withdrawn the ChangeRequest themselves
+        Reverted   = A moderator has "undone" the approval of a ChangeRequest and reverted data to how it was before
+        """
+        PENDING = 1
+        APPROVED = 2
+        DENIED = 3
+        WITHDRAWN = 4
+        REVERTED = 5
+        choices = (
+            (PENDING, 'Pending'),
+            (APPROVED, 'Approved'),
+            (DENIED, 'Denied'),
+            (WITHDRAWN, 'Withdrawn'),
+            (REVERTED, 'Reverted'),
+        )
+        lookup = dict((v[1].lower(), v[0]) for k, v in enumerate(choices))
+
+    # ChangeRequest does not support non-integer primary keys
+    object_type = models.ForeignKey(ContentType, related_name='%(class)s_object', on_delete=models.PROTECT)
+    object_id = models.PositiveIntegerField(null=True, blank=True)
+    object = GenericForeignKey('object_type', 'object_id')
+    object_str = models.CharField(max_length=250, blank=True)
+    related_type = models.ForeignKey(ContentType, null=True, blank=True, related_name='%(class)s_related',
+                                     on_delete=models.PROTECT)
+    request_type = models.PositiveSmallIntegerField(choices=Type.choices)
+    status = models.PositiveSmallIntegerField(choices=Status.choices, default=Status.PENDING)
+    data_revert = JSONField(encoder=DjangoJSONEncoder, null=True, blank=True)
+    data_changed = JSONField(encoder=DjangoJSONEncoder, null=True, blank=True)
+    comment = models.TextField(blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='%(class)s_user', on_delete=models.PROTECT)
+    user_ip = models.GenericIPAddressField(unpack_ipv4=True)
+    mod = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='%(class)s_mod', null=True, blank=True,
+                            on_delete=models.PROTECT)
+    mod_ip = models.GenericIPAddressField(unpack_ipv4=True, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, blank=True)
+    date_modified = models.DateTimeField(auto_now=True, blank=True)
+
+    def __str__(self):
+        return format_object_str(self.object_type, self.object_str, self.object_id)
+
+    @classmethod
+    def create(cls, obj: object, request_type: int = None) -> object:
+        """Creates a ChangeRequest object"""
+        cr = cls()
+        cr.set_object(obj)
+        cr.set_request_type(request_type)
+        cr.status = ChangeRequest.Status.PENDING
+        if cr.request_type != ChangeRequest.Type.RELATED:
+            cr.data_revert = obj.data_revert()
+            cr.data_changed = model_to_dict(obj)
+            if cr.request_type == ChangeRequest.Type.MODIFY:
+                fields = changed_keys(cr.data_revert, cr.data_changed)
+                if len(fields) > 0:  # Only filter data_revert & data_changed if there were changes found
+                    cr.data_revert = filter_data(cr.data_revert, fields)
+                    cr.data_changed = filter_data(cr.data_changed, fields)
+            elif cr.request_type == ChangeRequest.Type.DELETE:
+                cr.data_changed = None
+        # TODO: pass/set comment
+        cr.set_user(ChangeRequest.thread.request)
+        return cr
+
+    def set_object(self, obj):
+        if obj.pk:
+            # Existing object
+            self.object = obj
+        else:
+            # New object
+            self.object_type = ContentType.objects.get_for_model(obj)
+            self.object_id = None
+        self.object_str = str(obj)
+
+    def set_request_type(self, request_type: int = None):
+        if request_type is not None:
+            self.request_type = request_type
+        elif self.related_type is not None:
+            self.request_type = ChangeRequest.Type.RELATED
+        elif self.object is not None:
+            self.request_type = ChangeRequest.Type.MODIFY
+        else:
+            self.request_type = ChangeRequest.Type.ADD
+
+    def set_user(self, request: object):
+        self.user = request.user
+        self.user_ip = get_ip_from_request(request)
+
+    def set_mod(self, request: object):
+        self.mod = request.user
+        self.mod_ip = get_ip_from_request(request)
 
 
 class BaseHistoryModel(models.Model):
     """Adds logging features for for models with django-changerequest support"""
+    # Basic timestamp fields added to each model
+    date_created = models.DateTimeField(auto_now_add=True, blank=True)
+    date_modified = models.DateTimeField(auto_now=True, blank=True)
+
+    def data_revert(self):
+        """Obtains unaltered data (before save) from database"""
+        if self.pk:
+            data = self.__class__.objects.get(pk=self.pk)
+            return model_to_dict(data)
+        return None  # else
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
     class Meta:
         abstract = True
--- changerequest/utils.py
+++ changerequest/utils.py
@@ -2,8 +2,48 @@
 
 from urllib.request import parse_http_list
 
+from django.db.models.fields.related import ManyToManyField
+from django.db.models import FileField
+from django.forms.models import model_to_dict as _model_to_dict
 
-def get_ip_from_request(request):
+
+def format_object_str(object_type: str, object_str, object_id) -> str:
+    """Returns a string with object type and a string representation of the object and/or the primary key"""
+    result = '{}'.format(object_type)
+    if object_str:
+        result += ' "{}"'.format(object_str)
+    if object_id:
+        result += " ({})".format(object_id)
+    return result
+
+
+def model_to_dict(instance: object, exclude_pk: bool = True) -> dict:
+    """Converts model instance into a dictionary"""
+    raw = _model_to_dict(instance)
+    data = {}
+    for field in instance._meta.get_fields():
+        if field.name in raw:
+            if isinstance(field, ManyToManyField):
+                continue  # TODO: implement M2M field support
+            elif isinstance(field, FileField):
+                data[field.name] = str(raw[field.name])
+            elif not exclude_pk or field.name != instance._meta.pk.name:
+                data[field.name] = raw[field.name]
+    return data
+
+
+def changed_keys(a: dict, b: dict) -> list:
+    """Compares two dictionaries and returns list of keys where values are different"""
+    # Note! This function disregards keys that don't appear in both dictionaries
+    return [k for k in (a.keys() & b.keys()) if a[k] != b[k]]
+
+
+def filter_data(data: dict, keys: list) -> dict:
+    """Returns a dictionary with only key/value pairs where the key is in the keys list"""
+    return {k: data[k] for k in keys if k in data}
+
+
+def get_ip_from_request(request: object) -> str:
     """Extracts IP address from a request object"""
     # Code inspired by Flask's werkzeug/wrappers/base_request.py
     if 'HTTP_X_FORWARDED_FOR' in request.META:
